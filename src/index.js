7693e4a193abddb7509251f7e6e0c5e34b4043b4
YUNGU <yguhan@gmail.com>
2016-11-06 11:43:58 +0000

    react + book ?

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ class App extends Component {
 		super(props);
 		this.state = { 
 			videos: [], 
-			selectedVideo: null
+			selectedVideo: null	
 		};
 
 		this.videoSearch('surfboards');
@@ -34,7 +34,7 @@ class App extends Component {
 
 	render() {
 		const videoSearch = _.debounce((term) => {this.videoSearch(term) }, 300 );
-	
+
 		return (
 			<div>
 				<SearchBar onSearchTermChange={videoSearch} />
---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function(){
+	return [
+		{title: 'Javasciprt: The Good parts'},
+		{title: 'Harry Potter'},
+		{title: 'The Dark Tower'},
+		{title: 'Eloquent Ruby'},
+	];
+}
